b302cdbdf2374469031f5051786487707ee6cc14
pedrobergamini <41773103+pedrobergamini@users.noreply.github.com>
2023-09-27 17:22:46 -0300

    chore: create proxy

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -1,16 +1,16 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity >=0.8.19 <=0.9.0;
 
-
-import { ERC1967Proxy } from "@openzeppelin-upgradeable/proxy/ERC1967/ERC1967Proxy.sol";
+import { ERC1967Proxy } from "@openzeppelin/proxy/ERC1967/ERC1967Proxy.sol";
 import { DecentralizedExchange } from "src/DecentralizedExchange.sol";
 import { BaseScript } from "./Base.s.sol";
 
 /// @dev See the Solidity Scripting tutorial: https://book.getfoundry.sh/tutorials/solidity-scripting
 contract Deploy is BaseScript {
-    function run() public broadcast returns (Foo foo) {
-        DecentralizedExchange decentralizedExchange = new DecentralizedExchange();
-        ERC1967Proxy dexProxy = new ERC1967Proxy(address(decentralizedExchange), bytes(""));
+    function run() public broadcaster returns (DecentralizedExchange, ERC1967Proxy) {
+        DecentralizedExchange dexImplementation = new DecentralizedExchange();
+        ERC1967Proxy dexProxy = new ERC1967Proxy(address(dexImplementation), bytes(""));
 
+        return (dexImplementation, dexProxy);
     }
 }
--- src/DecentralizedExchange.sol
+++ src/DecentralizedExchange.sol
@@ -1,10 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.19;
 
+import { OwnableUpgradeable } from "@openzeppelin-upgradeable/access/OwnableUpgradeable.sol";
 import { UUPSUpgradeable } from "@openzeppelin-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 
-contract DecentralizedExchange is UUPSUpgradeable {
-    function foo() external view returns (uint256 bar) {
+contract DecentralizedExchange is UUPSUpgradeable, OwnableUpgradeable {
+    function foo() external pure returns (uint256 bar) {
         bar = 4;
     }
+
+    function _authorizeUpgrade(address) internal override onlyOwner { }
 }
